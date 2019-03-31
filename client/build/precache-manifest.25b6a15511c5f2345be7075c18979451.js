871f35963c3975a3da75783cd069d2b7d858666e
akira-19 <dounatsu00@yahoo.co.jp>
2019-03-31 10:30:00 -0700

    modified git ignore

---
+++ client/build/precache-manifest.25b6a15511c5f2345be7075c18979451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0faa6592e5a087c9fa85",
+    "url": "/AwardsLog/static/css/main.f4ac54de.chunk.css"
+  },
+  {
+    "revision": "0faa6592e5a087c9fa85",
+    "url": "/AwardsLog/static/js/main.0faa6592.chunk.js"
+  },
+  {
+    "revision": "fbd44bc81aa889813aa0",
+    "url": "/AwardsLog/static/js/1.fbd44bc8.chunk.js"
+  },
+  {
+    "revision": "2b0b936e5d8a5e18456b",
+    "url": "/AwardsLog/static/js/runtime~main.2b0b936e.js"
+  },
+  {
+    "revision": "af5c8aad4918825de5a262085307e392",
+    "url": "/AwardsLog/static/media/background.af5c8aad.jpg"
+  },
+  {
+    "revision": "ec02c65811a3b3f17f6918cb9e5292d2",
+    "url": "/AwardsLog/index.html"
+  }
+];
